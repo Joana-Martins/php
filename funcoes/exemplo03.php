4a15098769d73c4be589dd2dbff5a614e3422c02
Joana <joana.santoss.martins@gmail.com>
2022-06-08 09:55:11 -0300

    funções em php
    parâmetros, recursão e callback
    seção 7 OK

---
+++ funcoes/exemplo03.php
@@ -0,0 +1,12 @@
+<?php
+
+function ola ($texto = "mundo", $periodo = "Bom dia"){
+    return "Olá $texto! $periodo! <br>";
+}
+
+echo ola();
+echo ola("","Boa noite");
+echo ola("mundo", "Boa tarde");
+echo ola("Joana :)", "");
+
+?>
